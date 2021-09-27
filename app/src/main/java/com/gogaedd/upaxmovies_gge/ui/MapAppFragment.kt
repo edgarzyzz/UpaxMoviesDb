c3a1cbbeaf7f9297fe2da1df76f1c7bad3a226f5
egonzalez@actia.com.mx <todasmias+-17>
2021-09-27 12:32:54 -0500

    add map sdk here

--- app/src/main/java/com/gogaedd/upaxmovies_gge/ui/MapAppFragment.kt
+++ app/src/main/java/com/gogaedd/upaxmovies_gge/ui/MapAppFragment.kt
@@ -1,21 +1,36 @@
 package com.gogaedd.upaxmovies_gge.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.gogaedd.upaxmovies_gge.R
 import com.gogaedd.upaxmovies_gge.adapters.LocationAdapter
 import com.gogaedd.upaxmovies_gge.databinding.FragmentMapAppBinding
 import com.gogaedd.upaxmovies_gge.viewmodel.MapAppViewModel
+import kotlinx.android.synthetic.main.fragment_map_app.*
+import com.here.sdk.core.GeoCoordinates
+import com.here.sdk.mapview.*
+
+import com.here.sdk.mapview.MapScene.LoadSceneCallback
+import android.R
+
+import com.here.sdk.mapview.MapImageFactory
+
+import com.here.sdk.mapview.MapImage
+
+
+
+
 
 class MapAppFragment : Fragment() {
     lateinit var viewModel : MapAppViewModel
     lateinit var binding : FragmentMapAppBinding
     lateinit var adaperLocation : LocationAdapter
+    lateinit var mMapView: MapView
 
 
     override fun onCreateView(
@@ -35,6 +50,7 @@ class MapAppFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
         adaperLocation = LocationAdapter()
         binding.rvListLoc.apply {
             adapter = adaperLocation
@@ -43,7 +59,60 @@ class MapAppFragment : Fragment() {
 
         viewModel.startloadLocationsDevice()
 
+        mMapView = mapvApp
+        mMapView.onCreate(savedInstanceState)
+        loadMapScene()
+        startMap()
+
+    }
+    private fun startMap() {
+
+        mMapView.setOnReadyListener {
+            Log.d("TAG", "startMap: ")
+        }
+
     }
 
+
+    private fun loadMapScene() {
+        // Load a scene from the HERE SDK to render the map with a map scheme.
+        mMapView.mapScene.loadScene(MapScheme.NORMAL_DAY) { mapError ->
+            if (mapError == null) {
+                val distanceInMeters = (1000 * 10).toDouble()
+                mMapView.getCamera().lookAt(
+                    GeoCoordinates(19.432275,-99.133930), distanceInMeters
+                )
+//                startLoadMarkers()
+            } else {
+                Log.d("TAG", "Loading map failed: mapError: " + mapError.name)
+            }
+        }
+    }
+
+    private fun startLoadMarkers() {
+        val mapImage = MapImageFactory.fromResource(requireContext().resources, R.drawable.ic_delete)
+        viewModel.getLocations()?.let{listLoc->
+            listLoc.forEach{loc->
+                val mapMarker = MapMarker(GeoCoordinates(loc.lat.toDouble(), loc.long.toDouble()), mapImage)
+            }
+        }
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mMapView!!.onPause()
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        mMapView!!.onResume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mMapView!!.onDestroy()
+    }
 
 }
--- app/src/main/java/com/gogaedd/upaxmovies_gge/viewmodel/MapAppViewModel.kt
+++ app/src/main/java/com/gogaedd/upaxmovies_gge/viewmodel/MapAppViewModel.kt
@@ -2,6 +2,7 @@ package com.gogaedd.upaxmovies_gge.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import com.gogaedd.upaxmovies_gge.model.LocationApp
 import com.gogaedd.upaxmovies_gge.repository.MapAppRepository
 
 class MapAppViewModel(application: Application): AndroidViewModel(application) {
@@ -11,5 +12,10 @@ class MapAppViewModel(application: Application): AndroidViewModel(application) {
 
     fun startloadLocationsDevice(){
         repository.getDataFirestore()
+    }
+
+
+    fun getLocations(): MutableList<LocationApp>? {
+        return lvdListLocationsDevice.value
     }
 }
